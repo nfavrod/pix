fc7d69f4342d9b62acfd4d5ca4f232d49033e686
Jérémy Buget <jbuget@gmail.com>
2018-04-26 09:57:22 +0000

    Release of pf-12-publish-certification with env integration (via commit hash: 8e0aa0ecb9eb25bcd323a616b938c931ff062868)

--- live/pf-12-publish-certification/assets/pix-live.js
+++ live/pf-12-publish-certification/assets/pix-live.js
@@ -9287,6 +9287,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.47.0+a8846f44"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.47.0+8e0aa0ec"});
 }
 //# sourceMappingURL=pix-live.map
