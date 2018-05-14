7a3c0f8fbfeddda8d3d5529995e547a424464c27
Jérémy Buget <jbuget@gmail.com>
2018-05-14 13:42:22 +0000

    Release of tech-point-commit-message with env integration (via commit hash: bc7932c684cb6e885b161bea677c992c8d9dac29)

--- live/tech-point-commit-message/assets/pix-live.js
+++ live/tech-point-commit-message/assets/pix-live.js
@@ -9266,6 +9266,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.48.0+7eb3940f"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.48.0+bc7932c6"});
 }
 //# sourceMappingURL=pix-live.map
