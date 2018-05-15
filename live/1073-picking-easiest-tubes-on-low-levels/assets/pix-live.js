3bad5e7bab1acb37be23213ffa4e90849547d109
Jérémy Buget <jbuget@gmail.com>
2018-05-15 13:48:16 +0000

    Release of 1073-picking-easiest-tubes-on-low-levels with env integration (via commit hash: 7a7abe571ec89bc4f340bfe6866c13f522e5708c)

--- live/1073-picking-easiest-tubes-on-low-levels/assets/pix-live.js
+++ live/1073-picking-easiest-tubes-on-low-levels/assets/pix-live.js
@@ -9290,6 +9290,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.48.0+157c49e5"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.48.0+7a7abe57"});
 }
 //# sourceMappingURL=pix-live.map
