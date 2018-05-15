222fb4a02d239f1254671799a74d90c3bac39939
Jérémy Buget <jbuget@gmail.com>
2018-05-15 14:04:09 +0000

    Release of PF-57-add-url-to-start-smart-placement with env integration (via commit hash: 0a1d77deb10740b0ead91df7b1d30eb116ff9388)

--- live/PF-57-add-url-to-start-smart-placement/assets/pix-live.js
+++ live/PF-57-add-url-to-start-smart-placement/assets/pix-live.js
@@ -9335,6 +9335,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.48.0+b484c9ad"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.48.0+0a1d77de"});
 }
 //# sourceMappingURL=pix-live.map
