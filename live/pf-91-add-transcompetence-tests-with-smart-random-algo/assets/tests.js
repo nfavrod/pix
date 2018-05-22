fe65a13712ff7a614792e2fffad0faa3b0f35526
Jérémy Buget <jbuget@gmail.com>
2018-05-22 12:42:39 +0000

    Release of pf-91-add-transcompetence-tests-with-smart-random-algo with env integration (via commit hash: ad40231f04fe1ea66518dc9c3828c4ddd917681b)

--- live/pf-91-add-transcompetence-tests-with-smart-random-algo/assets/tests.js
+++ live/pf-91-add-transcompetence-tests-with-smart-random-algo/assets/tests.js
@@ -3780,6 +3780,10 @@ define('pix-live/tests/app.lint-test', [], function () {
       // test passed
     });
 
+    it('routes/campaigns/create-assessment.js', function () {
+      // test passed
+    });
+
     it('routes/certifications/results.js', function () {
       // test passed
     });
