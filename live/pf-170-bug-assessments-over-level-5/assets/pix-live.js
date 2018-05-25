b84f4c48c21b7a5b4c36af0e39eebecde4e64860
Jérémy Buget <jbuget@gmail.com>
2018-05-25 14:28:37 +0000

    Release of pf-170-bug-assessments-over-level-5 with env integration (via commit hash: b940b19e311ca66948ce38cd2ec72a8c43227ad4)

--- live/pf-170-bug-assessments-over-level-5/assets/pix-live.js
+++ live/pf-170-bug-assessments-over-level-5/assets/pix-live.js
@@ -9418,6 +9418,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.49.0+0f611983"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.49.0+b940b19e"});
 }
 //# sourceMappingURL=pix-live.map
