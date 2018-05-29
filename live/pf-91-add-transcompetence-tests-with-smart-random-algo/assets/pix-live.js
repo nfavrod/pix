df280af6a46027b736eddd8eaea5735568702c71
Jérémy Buget <jbuget@gmail.com>
2018-05-29 12:35:35 +0000

    Release of pf-91-add-transcompetence-tests-with-smart-random-algo with env integration (via commit hash: 23810d4734231dd3414493ed7f9887ee58c2bc1c)

--- live/pf-91-add-transcompetence-tests-with-smart-random-algo/assets/pix-live.js
+++ live/pf-91-add-transcompetence-tests-with-smart-random-algo/assets/pix-live.js
@@ -9413,6 +9413,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.49.3+d2946b22"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.49.3+23810d47"});
 }
 //# sourceMappingURL=pix-live.map
