db783c7fd3a3b9651b337160d9cea6868515d24c
Jérémy Buget <jbuget@gmail.com>
2018-05-29 14:32:29 +0000

    Release of pf-189-constructor-object-assign-in-deprecated-method with env integration (via commit hash: ac497d9a755daeb42a47c3b93efd4918d282cbdb)

--- live/pf-189-constructor-object-assign-in-deprecated-method/assets/pix-live.js
+++ live/pf-189-constructor-object-assign-in-deprecated-method/assets/pix-live.js
@@ -9413,6 +9413,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.49.3+08ced7da"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.49.3+ac497d9a"});
 }
 //# sourceMappingURL=pix-live.map
