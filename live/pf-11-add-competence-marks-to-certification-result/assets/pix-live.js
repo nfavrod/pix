817f5aeeb1b9bbe6c0c622fc0d63b28c1f4226b4
Jérémy Buget <jbuget@gmail.com>
2018-06-06 08:43:21 +0000

    Release of pf-11-add-competence-marks-to-certification-result with env integration (via commit hash: 00f181ebde4614b196b2f50528672442f50de948)

--- live/pf-11-add-competence-marks-to-certification-result/assets/pix-live.js
+++ live/pf-11-add-competence-marks-to-certification-result/assets/pix-live.js
@@ -9795,6 +9795,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT":5,"name":"pix-live","version":"1.50.0+45b10503"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT":5,"name":"pix-live","version":"1.50.0+00f181eb"});
 }
 //# sourceMappingURL=pix-live.map
