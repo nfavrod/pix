e7232209188512c0d4839f7b139f191d26713d7f
Jérémy Buget <jbuget@gmail.com>
2018-07-03 14:53:34 +0000

    Release of pf-40-seconde-chance-modale with env integration (via commit hash: 8f1b747a41e39071de0fec18c96c58a0390a4665)

--- live/pf-40-seconde-chance-modale/assets/pix-live.js
+++ live/pf-40-seconde-chance-modale/assets/pix-live.js
@@ -963,9 +963,6 @@ define('pix-live/components/competence-level-progress-bar', ['exports'], functio
       },
       closeModal: function closeModal() {
         this.set('_showSecondChanceModal', false);
-      },
-      modalValidated: function modalValidated() {
-        this.sendAction('createAssessment', this.get('courseId'));
       }
     }
   });
@@ -9902,6 +9899,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"useDelay":true,"NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT":5,"name":"pix-live","version":"1.52.0+aeb5b8e4"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"useDelay":true,"NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT":5,"name":"pix-live","version":"1.52.0+8f1b747a"});
 }
 //# sourceMappingURL=pix-live.map
