c09b9a837ebe25af09208381eec40aec18249e40
Jonathan Perret <jonathan@ut7.fr>
2018-07-11 17:37:50 +0200

    Don't rewrite and push the whole Git history to Dokku

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -8,10 +8,22 @@ case $BUILD_ENV in
   "integration")
     if [ -n "$GIT_BRANCH" ]
     then
-      git remote add dokku dokku@pix-app.ovh:${GIT_BRANCH}
       cd ..
-      git filter-branch --prune-empty --subdirectory-filter api HEAD
-      git push --force dokku HEAD:master 
+      # Save HEAD commit SHA1
+      current_sha1=$(git rev-parse HEAD)
+
+      # Create new orphan branch (no commits)
+      git branch -D temporary-branch-for-deployment 2>/dev/null || true
+      git checkout --orphan temporary-branch-for-deployment
+
+      # Commit current index, quietly, skipping commit hooks, reusing commit info from previous HEAD
+      git commit --quiet --no-verify -C${current_sha1}
+
+      # Replace the root with the contents of api/ (will only rewrite one commit)
+      git filter-branch --force --prune-empty --subdirectory-filter api temporary-branch-for-deployment
+
+      # Push our streamlined commit to dokku
+      git push --force dokku@pix-app.ovh:${GIT_BRANCH} temporary-branch-for-deployment:master
     fi
   ;;
   "staging")
